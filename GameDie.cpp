9e7ba3156737f143d4fa22ff9f15cf63d6e777ae
Abhinav Gupta <abhi.951990@gmail.com>
2016-01-31 18:18:19 -0800

    #1 resolved issue

--- GameDie.cpp
+++ GameDie.cpp
@@ -7,15 +7,15 @@ using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
-  roll();
+  cout<<roll()<<endl;
 }
 
 //generate a random number between 1-6 (inclusive) and display it
-void GameDie::roll()
+int GameDie::roll()
 {
 
 srand (time(NULL));
 int a= rand()%6 +1;
-cout<<a<<endl;
-
+// cout<<a<<endl;
+return a;
 }
