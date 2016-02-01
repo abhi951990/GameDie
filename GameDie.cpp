0c54cfe66b0f34b4a93e0f4d51f304d5f714e53d
Abhinav Gupta <abhi.951990@gmail.com>
2016-01-31 17:31:55 -0800

    first commit

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <cstdlib>
+#include "GameDie.h"
+#include <time.h>
 
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+  roll();
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+
+srand (time(NULL));
+int a= rand()%6 +1;
+cout<<a<<endl;
+
 }
